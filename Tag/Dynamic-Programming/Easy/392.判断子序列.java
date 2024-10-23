8a72bb04064ef2c40b83a431593384db64f34900
luyanhao <luyanhao@xiaomi.com>
2024-10-23 19:49:08 +0800

    [392] 判断子序列

--- Tag/Dynamic-Programming/Easy/392.判断子序列.java
+++ Tag/Dynamic-Programming/Easy/392.判断子序列.java
@@ -7,7 +7,18 @@
 // @lc code=start
 class Solution {
     public boolean isSubsequence(String s, String t) {
+        int i = 0; 
+        int j = 0;
+        while (i < s.length() && j < t.length()) {
+            char ci = s.charAt(i);
+            char cj = t.charAt(j);
+            if (ci == cj) {
+                i ++;
+            }
+            j ++;
+        }
 
+        return i == s.length();
     }
 }
 // @lc code=end
